d70472af10607d46f71856275430164a0bb42c6e
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-25 02:42:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length >= 1
+      new_array[]
+      counter = 1
+      array.each do |name|
+        new_array.push("#{counter}. #{new_array}")
+        counter +=1 
+      end
+      puts 
+  
