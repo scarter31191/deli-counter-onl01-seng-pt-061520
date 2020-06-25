0c9440d2fd8204107cf8bdad7e7f0ca9450d2a27
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-25 02:57:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,11 @@ def line(array)
     end
   end
   
+ def take_a_number(line, new_person)
+   line << "new_person"
+   puts "Welcome #{new_person} you are number #{line.length} in line"
+ end
+  
+  
+  
+  
