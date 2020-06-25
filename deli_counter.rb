9a81f168fe1a7eb6944d21164ff3fe62015b089e
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-25 03:22:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
   if array.length >= 1
-      new_array[]
+      new_array = []
       counter = 1
       array.each do |name|
         new_array.push("#{counter}. #{new_array}")
