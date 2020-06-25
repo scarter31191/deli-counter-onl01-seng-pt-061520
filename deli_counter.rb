6e58c48d4d6f418b2c404c740fee8a42d7498636
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-25 03:38:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,36 +20,10 @@ def line(array)
   end 
 end 
 
-
-
-# def line(array)
-#   if array.length >= 1
-#       new_array = []
-#       counter = 1
-#       array.each do |name|
-#         new_array.push("#{counter}. #{new_array}")
-#         counter +=1 
-#       end
-#       puts "The line is currently: #{new_array.join (" ")}"
-#     else
-#       puts "The line is currently empty."
-#     end
-#   end
-  
-# def take_a_number(line, new_person)
-#   line.push(new_person)
-#   puts "Welcome, #{new_person}. you are number #{line.length} in line."
-# end
- 
-# def now_serving(line)
-#   if line.length == 0
-#       puts "There is nobody waiting to be served!" 
-#   else
-#     puts "Currently serving #{line[0]}."
-#     line.shift
-#   end
-# end
-  
-  
-  
-  
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
